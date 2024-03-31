a55a64c3d0da4a09fe3215f4e7697d8fd177d47f
Saon00 <sikdersaon1@gmail.com>
2024-04-01 01:13:03 +0600

    new apps added

--- playground/urls.py
+++ playground/urls.py
@@ -5,6 +5,7 @@ from . import views
 # URL configuration
 urlpatterns = [
     # we don't need
-    path('kire/', views.say_hello)
+    path('kire/', views.say_hello),
+    path('add-korchi/', views.add)
     # path('playground/hello/', views.say_hello)
 ]
--- playground/views.py
+++ playground/views.py
@@ -2,5 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
-def say_hello(request):
-    return HttpResponse('Saon SIkder')
+def say_hello(req):
+    return render(req, 'index.html')
+    # return HttpResponse('Saon Srabon')
+
+def add(request):
+    return HttpResponse("1+1")
